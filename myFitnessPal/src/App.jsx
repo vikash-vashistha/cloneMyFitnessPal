a06c068bf62a472aeada1c59a5566f765c221367
K.M.ShivaGoud <maheshshivagoud@gmail.com>
2022-03-30 21:53:46 +0530

    css back image change

--- myFitnessPal/src/App.jsx
+++ myFitnessPal/src/App.jsx
@@ -9,7 +9,8 @@ function App() {
 
   return (
     <div className="App">
-      <AllApps />
+      {/* <AllApps /> */}
+      <Community />
     </div>
   )
 }
--- myFitnessPal/src/main.jsx
+++ myFitnessPal/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { Community } from './Components/community/community'
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    
+
   </React.StrictMode>,
   document.getElementById('root')
 )
