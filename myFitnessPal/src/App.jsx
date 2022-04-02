b51d19035e952fa9884516814772e66097e1394c
skale0181 <skale0181@gmail.com>
2022-04-02 10:16:41 +0530

    done

--- myFitnessPal/src/App.jsx
+++ myFitnessPal/src/App.jsx
@@ -1,17 +1,17 @@
 import { useState } from 'react'
 import './App.css'
-import AfterExrecise from './Components/AfterExercise/AfterExrecise'
+
 import AfterHome from './Components/AfterHome/AfterHome'
 import BeforeAbout from './Components/BeforeAbout/BeforeAbout'
-import BeforeFood from './Components/BeforeFood/BeforeFood'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      {/* <BeforeAbout/> */}
-      {/* <AfterHome/> */}
+      <BeforeAbout/>
+      <AfterHome/>
       {/* <BeforeFood/> */}
     </div>
   )
